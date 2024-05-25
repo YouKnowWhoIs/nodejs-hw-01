626f26fa8cfd91b925ac50e85bdc6a2a6a16ae48
YouKnowWhoIs <leokatmax@gmail.com>
2024-05-25 14:11:09 +0300

    up

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,9 @@
+import path from 'node:path';
+
+import { PATH_DB } from '../constants/contacts.js';
+
+const generateContacts = async (number) => {
+  // const createFakeContact = path.join(process.cwd());
+};
+
+await generateContacts(5);
